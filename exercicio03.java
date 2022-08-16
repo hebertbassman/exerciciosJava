af172bc14a93aa3709882b83972f24ccb95c09a3
Hebert Silva <109643160+hebertbassman@users.noreply.github.com>
2022-08-15 23:13:15 -0300

    Add files via upload
    Um programa que lê quatro valores inteiros, A, B, C e D. A seguir, calcula e mostra a diferença do produto de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).

---
+++ exercicio03.java
@@ -0,0 +1,27 @@
+package exercicios_sessao4_estruturaSequencial;
+
+import java.util.Scanner;
+
+public class exercicio03 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Insira o valor A: ");
+		int A = sc.nextInt();
+		System.out.print("Insira o valor B: ");
+		int B = sc.nextInt();
+		System.out.print("Insira o valor C: ");
+		int C = sc.nextInt();
+		System.out.print("Insira o valor D: ");
+		int D = sc.nextInt();
+		
+		int diferenca = (A * B) - (C * D);
+		
+		System.out.println("DIFERENÇA = " + diferenca);
+		
+		sc.close();
+
+	}
+}
