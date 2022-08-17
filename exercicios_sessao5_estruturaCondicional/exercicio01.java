e6f7040a826b86e6d98f1b59a939d9bc9497c273
Hebert Silva <109643160+hebertbassman@users.noreply.github.com>
2022-08-16 21:40:28 -0300

    Update exercicio01.java

--- exercicios_sessao5_estruturaCondicional/exercicio01.java
+++ exercicios_sessao5_estruturaCondicional/exercicio01.java
@@ -6,19 +6,18 @@ import java.util.Scanner;
 public class exercicio01 {
 
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		int num = sc.nextInt();
-		
+
 		if (num < 0) {
 			System.out.print("Negativo!");
+		} else {
+			System.out.print("Não negativo!");
 		}
-			else {
-				System.out.print("Não negativo!");
-			}
-			
-			sc.close();
-		}
+
+		sc.close();
 	}
+}
