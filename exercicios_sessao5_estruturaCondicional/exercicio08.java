ed5a19e60f923ea7bc860eac3e7851dd073f6261
Hebert Silva <109643160+hebertbassman@users.noreply.github.com>
2022-08-18 00:30:33 -0300

    Add files via upload
    Um programa que lê um valor com duas casas decimais, equivalente ao salário de uma pessoa. Em seguida, calcula e mostra o valor que esta pessoa deve pagar de imposto de renda sendo que, de R$ 0 até R$ 2000.00, o imposto é isento, de R$ 2000.01 até R$ 3000.00, o imposto é de 8%, de R$ 3000.01 até R$ 4500.00, o imposto é de 18% e de R$ 4500.00 em diante, o imposto é de 28%. Obs: O programa leva em conta a faixa salarial pertinente aos impostos.

---
+++ exercicios_sessao5_estruturaCondicional/exercicio08.java
@@ -0,0 +1,31 @@
+package exercicios_sessao5_estruturaCondicional;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class exercicio08 {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Salário: ");
+		double salario = sc.nextDouble();
+		double imposto = 0;
+
+		if (salario >= 0 && salario <= 2000.00) {
+			System.out.println("Isento");
+		} else if (salario > 2000.00 && salario <= 3000.00) {
+			imposto = (salario - 2000.00) * 8 / 100;
+		} else if (salario > 3000.00 && salario <= 4500.00) {
+			imposto = (salario - 3000.00) * 18 / 100 + 1000.00 * 8 / 100;
+		} else {
+			imposto = (salario - 4500.00) * 28 / 100 + 1500.00 * 18 / 100 + 1000.00 * 8 / 100;
+		}
+
+		System.out.printf("Valor do imposto: %.2f%n", imposto);
+
+		sc.close();
+	}
+}
