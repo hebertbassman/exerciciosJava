d559479fde7b72952a701ab542648aa9b1efefa5
Hebert Silva <109643160+hebertbassman@users.noreply.github.com>
2022-08-21 23:20:29 -0300

    Add files via upload
    Um programa que lê um número n depois lê n pares de números e mostra a divisão do primeiro pelo segundo. Se o denominador for igual a zero, mostra a mensagem "Divisão impossível!".

---
+++ exercicios_secao06_estruturasRepetitivas/Exercicio07.java
@@ -0,0 +1,32 @@
+package exercicios_secao06_estruturasRepetitivas;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio07 {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Insira o valor n: ");
+		double n = sc.nextInt();
+
+		for (int i = 0; i < n; i++) {
+			System.out.print("Insira pares de números: ");
+
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+			if (x == 0 || y == 0) {
+				System.out.println("Divisão impossível!");
+			} 
+			else {
+				double result = (double) x / y; // (double) converte o resultado de x / y em double
+				System.out.printf("%.1f%n", result);
+			}
+		}
+
+		sc.close();
+	}
+}
